e1f58f47a6d1c72cd71009dbc1d1b9dd31428b3c
matdombrock <dombrock.mathieu@gmail.com>
2025-11-08 13:10:09 -0800

    tbox ui

--- tbox/tbox.fish
+++ tbox/tbox.fish
@@ -15,7 +15,8 @@ end
 
 set query_str $argv
 
-set back_str " back"
+set back_str "back "
+set exit_str "exit "
 set fzf_opts --height=80% --layout=reverse --border --ansi
 
 set cmds
@@ -42,6 +43,11 @@ end
 
 function expand_categories
     set keys (string split '||' $categories)
+    # Exit option
+    echo -n (set_color red)
+    echo $exit_str
+    # All option
+    echo -n (set_color normal)
     echo all
     for key in $keys
         echo $key
@@ -50,8 +56,15 @@ end
 
 function expand_cmds
     set category $argv[1]
-    set_color red
-    echo $back_str
+    set is_single_cat $argv[2]
+    # Exit option
+    echo -n (set_color red)
+    echo $exit_str
+    # Back option 
+    if not test $is_single_cat = true
+        echo -n (set_color blue)
+        echo $back_str
+    end
     # All category
     if test $category = all
         set keys (string split '||' $categories)
@@ -87,26 +100,48 @@ else
 end
 source $cmd_path
 
-function run
-    set cat_str
+function header
+    if not set -q TBOX_NO_HEADER; or test $TBOX_NO_HEADER != 1
+        echo "\
+$(set_color blue   )    █$(set_color green )▗▄▄▄▖▗▄▄▖  ▗▄▖ ▗▖  $(set_color blue   ) ▗▖
+$(set_color magenta)  ███$(set_color yellow)  █  ▐▌ ▐▌▐▌ ▐▌ ▝▚▞$(set_color magenta)▞▘ 
+$(set_color magenta) ██ █$(set_color yellow)  █  ▐▛▀▚▖▐▌ ▐▌  ▐▌$(set_color magenta)▌  
+$(set_color blue   )  ███$(set_color green )  █  ▐▙▄▞▘▝▚▄▞▘▗▞▘▝$(set_color blue   )▝▚▖
+" | string replace -a ' ' \~
+    end
+end
 
+function run
+    header
+    set is_single_cat false
+    set cat_str
     # If we have a prefilled command query, skip category selection
     if test -n "$query_str"
         set cat_str all
         # If we have only one category, skip category selection
     else if test (count (string split '||' $categories)) -eq 1
         set cat_str (string split '||' $categories)[1]
+        set is_single_cat true
     else
         set cat_str (expand_categories | fzf $fzf_opts --prompt="$(set_color blue)category > ")
+        # check for exit
         if test -z "$cat_str"
-            exit 1
+            return
+        end
+        if test $cat_str = $exit_str
+            return
         end
     end
 
-    set cmd_str (expand_cmds $cat_str | fzf --query=$query_str $fzf_opts --prompt="$(set_color green)$cat_str/$(set_color blue)command > " --preview 'echo {}' --preview-window down:5:hidden:wrap --bind '?:toggle-preview')
+    set cmd_str (expand_cmds $cat_str $is_single_cat | fzf --query=$query_str $fzf_opts --prompt="$(set_color green)$cat_str/$(set_color blue)command > " --preview 'echo {}' --preview-window down:5:hidden:wrap --bind '?:toggle-preview')
 
     if test -z "$cmd_str"
-        exit 1
+        return
+    end
+
+    # check for exit
+    if test $cmd_str = $exit_str
+        return
     end
 
     # check for back
@@ -160,15 +195,6 @@ function run
     echo λ $cmd
 
     eval $cmd
-end
-
-if not set -q TBOX_NO_HEADER; or test $TBOX_NO_HEADER != 1
-    echo "\
-$(set_color blue   )    █$(set_color green )▗▄▄▄▖▗▄▄▖  ▗▄▖ ▗▖  $(set_color blue   ) ▗▖
-$(set_color magenta)  ███$(set_color yellow)  █  ▐▌ ▐▌▐▌ ▐▌ ▝▚▞$(set_color magenta)▞▘ 
-$(set_color magenta) ██ █$(set_color yellow)  █  ▐▛▀▚▖▐▌ ▐▌  ▐▌$(set_color magenta)▌  
-$(set_color blue   )  ███$(set_color green )  █  ▐▙▄▞▘▝▚▄▞▘▗▞▘▝$(set_color blue   )▝▚▖
-" | string replace -a ' ' \~
 end
 
 run
