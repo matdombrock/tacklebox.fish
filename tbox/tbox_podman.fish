34d209f8a040da6bc0f8132518de109705a49221
matdombrock <dombrock.mathieu@gmail.com>
2025-11-11 14:23:27 -0800

    tbox podman

--- tbox/tbox_podman.fish
+++ tbox/tbox_podman.fish
@@ -1,5 +1,23 @@
-add_cmd pm "list containers" "podman container ls"
-add_cmd pm "list all containers" "podman container ls -a"
-add_cmd pm "list images" "podman image ls"
-add_cmd pm "list volumes" "podman volume ls"
-add_cmd pm "list pods" "podman pod ls"
+set runtime ""
+# Check if we have docker installed
+if type -q docker
+    set runtime docker
+else if type -q podman
+    set runtime podman
+end
+# If we have neither, exit
+if test -z "$runtime"
+    echo "No container runtime (docker or podman) found."
+    return
+end
+
+add_cmd "list (ls)" "list containers" "$runtime container ls"
+add_cmd "list (ls)" "list all containers" "$runtime container ls -a"
+add_cmd "list (ls)" "list images" "$runtime image ls"
+add_cmd "list (ls)" "list volumes" "$runtime volume ls"
+
+add_cmd "delete (rm)" "remove container" "$runtime container rm {#}"
+add_cmd "delete (rm)" "remove image" "$runtime image rm {#}"
+add_cmd "delete (rm)" "remove all unused images" "$runtime image prune -a"
+add_cmd "delete (rm)" "remove all dangling images" "$runtime image prune"
+add_cmd "delete (rm)" "remove all unused volumes" "$runtime volume prune"
