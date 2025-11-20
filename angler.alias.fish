4ab5011458fabc95bf76a555a0eb27be03ad892d
matdombrock <dombrock.mathieu@gmail.com>
2025-11-20 04:36:30 -0800

    alias

--- angler.alias.fish
+++ angler.alias.fish
@@ -1,56 +1,40 @@
 #! /usr/bin/env fish
 
 # Set aliases for some programs in this repo
-
-# NOTE: these aliases are prefixed with a colon \(:\) to avoid conflicts with existing commands.
+# NOTE: 
+# these aliases are prefixed with a colon \(:\) to avoid conflicts with existing commands.
+#
+# Usage:
+# source ~/path/to/angler.fish/angler.alias.fish
 
 # Automatically determine the base dir
 set -l base (dirname (realpath (status --current-filename)))
 
-set helptext "$(set_color brblue)Available Angler aliases:\n"
-function helpa
-    set -l split (string split ' - ' $argv)
-    set -a helptext "$(set_color brgreen)$split[1] - $(set_color bryellow)$split[2] \n"
-end
-
 #
 # Sourced files
+# 
+# Some files need to be sourced to work properly
 #
-
-# Rod prompt manager
 source $base/rod/rod.fish
-helpa ":rod - Manage Rod prompt styles."
 
-#
-# Standard aliases
-#
+# Helper to create aliases and build help text
+set _angler_alias_help_text "$(set_color brblue)Available Angler aliases:\n"
+function aalias
+    set -l desc $argv[1]
+    set -l name $argv[2]
+    set -l cmd $argv[3]
+    set -a _angler_alias_help_text "$(set_color brgreen)$name - $(set_color bryellow)$desc \n"
+    alias $name=$cmd
+end
 
-# Adventure game
-alias :adv="$base/games/adv.fish"
-helpa ":adv - Launch the Adventure game."
-# Launch FishFinder (fuzzy file explorer)
-alias :fishfinder="$base/fishfinder/finder.fish"
-helpa ":fishfinder - Launch the FishFinder fuzzy file explorer."
-alias :ff="$base/fishfinder/finder.fish"
-helpa ":ff - Launch the FishFinder fuzzy file explorer."
-# Launch fish finder and go to the final dir
-alias :ffg=":ff && cd ($base/fishfinder/finder.fish l)"
-helpa ":ffg - Launch FishFinder and cd to the selected directory."
-# Demo TackleBox
-alias :tbox="$base/tbox/tbox.fish"
-helpa ":tbox - Launch the TackleBox demo."
-# Fish in Fish
-alias :fishfish="$base/fishfish/fish.fish"
-helpa ":fishfish - Launch Fish in Fish."
-# Launch Reel (fuzzy package manager)
-alias :reel="sudo $base/reel/reel.fish"
-helpa ":reel - Launch the Reel fuzzy package manager."
-# Quick git commits
-alias :fgit="$base/fgit/fgit.fish"
-helpa ":fgit - Quick git commit helper."
-# Launch angler tbox
-alias :angler="$base/angler.fish"
-helpa ":angler - Launch the Angler TackleBox."
-# List aliases
-alias :alias="echo -e \$helptext"
-helpa ":alias - List all Angler aliases."
+aalias "List all Angler aliases" :alias "echo -e \$_angler_alias_help_text"
+aalias "Angler TackleBox" :angler "$base/angler.fish"
+aalias "FishFinder fuzzy file explorer" :fishfinder "$base/fishfinder/finder.fish"
+aalias "FishFinder fuzzy file explorer" :ff "$base/fishfinder/finder.fish"
+aalias "Run FishFinder and cd to the selected directory" :ffg ":ff && cd ($base/fishfinder/finder.fish l)"
+aalias "TackleBox command launcher" :tbox "$base/tbox/tbox.fish"
+aalias "Fish in Fish" :fishfish "$base/fishfish/fish.fish"
+aalias "Reel fuzzy package manager" :reel "$base/reel/reel.fish"
+aalias "Quick git commit helper" :fgit "$base/fgit/fgit.fish"
+aalias "Manage fish shell prompt styles" :rod _rod
+aalias "Adventure game" :adv "$base/games/adv.fish"
